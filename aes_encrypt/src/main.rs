5c67b6c47d38c831df3868e1128f152dac7a1f91
atlomak <andrzej.tlomak01@gmail.com>
2024-04-22 00:20:14 +0200

    feat: working aes poc

--- aes_encrypt/src/main.rs
+++ aes_encrypt/src/main.rs
@@ -1,20 +1,20 @@
-use std::{fs::{read_to_string, File}, io::Write};
+use std::{
+    fs::{self, File},
+    io::Write,
+};
 
 use aes_gcm::{
-    aead::{Aead, AeadCore, KeyInit, OsRng}, Aes128Gcm, Key
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    Aes128Gcm, Key, Nonce,
 };
 
 #[derive(Debug)]
 struct CipherData {
-    cipher: Vec<u8>,
+    cipher_text: Vec<u8>,
     key: Vec<u8>,
-    nonce: Vec<u8>
+    nonce: Vec<u8>,
 }
 fn main() {
-    let filename = "resources/test-file.txt";
-
-    let plaintext = read_to_string(filename).expect(&filename);
-
 }
 
 fn encrypt(plaintext: &str) -> CipherData {
@@ -25,22 +25,51 @@ fn encrypt(plaintext: &str) -> CipherData {
     let cipher = Aes128Gcm::new(&key);
     let nonce = Aes128Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
 
-    let ciphertext = cipher.encrypt(&nonce, plaintext.as_ref()).expect("Error while encrypting data");
+    let ciphertext = cipher
+        .encrypt(&nonce, plaintext.as_ref())
+        .expect("Error while encrypting data");
 
-    CipherData{
-        cipher: ciphertext,
+    CipherData {
+        cipher_text: ciphertext,
         key: key.to_vec(),
-        nonce: nonce.to_vec()
+        nonce: nonce.to_vec(),
     }
 }
 
-fn save_to_file(cipherdata: &CipherData) {
-    let mut file = File::create("key.bin").unwrap();
+fn decrypt(encrypted_data: CipherData) -> Vec<u8> {
+    let key = Key::<Aes128Gcm>::from_slice(&encrypted_data.key);
+    let cipher = Aes128Gcm::new(&key);
+    let nonce = Nonce::from_slice(&encrypted_data.nonce);
+    let cipher_text = encrypted_data.cipher_text;
+
+    cipher.decrypt(nonce, cipher_text.as_ref()).unwrap()
+}
+
+fn save_to_file(cipherdata: &CipherData, resources_path: String) {
+    let mut file = File::create(resources_path.clone() + "key.bin").unwrap();
     file.write_all(&cipherdata.key).unwrap();
-    let mut file = File::create("nonce.bin").unwrap();
+
+    let mut file = File::create(resources_path.clone() + "nonce.bin").unwrap();
     file.write_all(&cipherdata.nonce).unwrap();
-    let mut file = File::create("cipher.bin").unwrap();
-    file.write_all(&cipherdata.cipher).unwrap();
+
+    let mut file = File::create(resources_path.clone() + "cipher.bin").unwrap();
+    file.write_all(&cipherdata.cipher_text).unwrap();
+}
+
+fn cipher_from_files(resources_path: String) -> CipherData {
+    let cipher_path = resources_path.clone() + "cipher.bin";
+    let key_path = resources_path.clone() + "key.bin";
+    let nonce_path = resources_path.clone() + "nonce.bin";
+
+    let cipher_text = fs::read(cipher_path).expect("Cant read file with cipher");
+    let key = fs::read(key_path).expect("Cant read file with key");
+    let nonce = fs::read(nonce_path).expect("Cant read file with nonce");
+
+    CipherData {
+        cipher_text,
+        key,
+        nonce,
+    }
 }
 
 #[test]
@@ -49,9 +78,21 @@ fn test_encrypt() {
 
     print!("Plaintext: {:?}\n", plaintext.as_bytes());
 
-    let cipherData = encrypt(plaintext);
+    let cipher_data = encrypt(plaintext);
 
-    save_to_file(&cipherData);
+    let resources_path = String::from("resources/");
 
-    print!("{:?}\n", cipherData);
+    save_to_file(&cipher_data, resources_path);
+
+    print!("{:?}\n", cipher_data);
+}
+
+#[test]
+fn test_read_and_decrypt() {
+    let resources_path = String::from("resources/");
+    let encrypted_data = cipher_from_files(resources_path);
+
+    let plaintext = decrypt(encrypted_data);
+
+    assert_eq!(&plaintext, b"Hello world");
 }
